e2ab9efefb03b04ff5f70d77c914bb97650570e0
Thomas Eckerstorfer <eckerstorfer.thomas@gmail.com>
2015-10-10 17:33:00 +0200

    drag and drop changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -87,5 +87,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         name: 'TESTPayment Info',
         icon: 'payment'
     }];
+    app.selecteditem = function() { return 0; }
 
 })(document);
