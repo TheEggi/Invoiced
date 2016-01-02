a028f6ff132f9d633e2643b365e643224d54949f
Thomas Eckerstorfer <eckerstorfer.thomas@gmail.com>
2016-01-02 18:09:53 +0100

    changed alignements

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,6 @@
  * Require Browsersync along with webpack and middleware for it
  */
 var browserSync = require('browser-sync');
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-
-/**
- * Require ./webpack.config.js and make a bundler from it
- */
-var webpackConfig = require('./webpack.config');
-var bundler = webpack(webpackConfig);
 
 /**
  * Run Browsersync and use middleware for Hot Module Replacement
